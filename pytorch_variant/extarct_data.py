c238f6b560154260b18343eaadbb8234300829de
rahul96rajan <r96rajan@gmail.com>
2021-01-16 22:11:19 +0530

    added extract_data.py

---
+++ pytorch_variant/extarct_data.py
@@ -0,0 +1,14 @@
+"""
+Expect IAM online handwriting dataset to be placed under ./data/
+in the following order
+    text-2-strokes
+    │
+    └──data
+        |
+        └──original-xml-part
+            | a01
+            | a02
+            | a03
+            | ...
+"""
+
