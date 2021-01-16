78cc55b60e4b46629bef0118a9b7342e254b4c4a
rahul96rajan <r96rajan@gmail.com>
2021-01-16 22:31:14 +0530

    added extractor method

--- pytorch_variant/extarct_data.py
+++ pytorch_variant/extarct_data.py
@@ -14,7 +14,6 @@ in the following order
                 | ...
 """
 
-import os
 import glob
 import xml.etree.ElementTree as ElementTree
 import html
@@ -56,3 +55,69 @@ def change_coord_to_offsets(stroke, norm_factor=20):
     stroke_offed[:, 2] = - stroke_offed[:, 2]
 
     return np.vstack(([0, 0, 0], stroke_offed)).astype('float32')
+
+
+def extract_data(path='./data/original-xml-part/'):
+    files_paths = glob.glob(path + '**/*.xml', recursive=True)
+    file_no = 0
+    sentences_all = []
+    strokes_all = []
+    for file in files_paths:
+        file_no += 1
+        xml = ElementTree.parse(file).getroot()
+        transcription = xml.findall('Transcription')
+        if not transcription:
+            print(f'[INFO] Skipped file {file}')
+            continue
+        textslines = [html.unescape(tl.get('text'))
+                      for tl in transcription[0].findall('TextLine')]
+        points_set = [strk.findall('Point')
+                      for strk in xml.findall('StrokeSet/Stroke')]
+        parsed_points_set = []
+        mid_points = []
+        for points in points_set:
+            parsed_points = [[0, int(pt.get('x')), int(pt.get('y'))]
+                             for pt in points]
+            parsed_points[-1][0] = 1
+            parsed_points_set.append(parsed_points)
+            mid_points.append(get_midpoints(parsed_points))
+
+        distances = [-(abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]))
+                     for p1, p2 in zip(mid_points, mid_points[1:])]
+        splits = sorted(np.argsort(distances)[:len(textslines) - 1] + 1)
+
+        strokes_wrt_textlines = []
+        for strt_idx, end_idx in zip([0] + splits,
+                                     splits + [len(parsed_points_set)]):
+            strokes_wrt_textlines.append(parsed_points_set[strt_idx: end_idx])
+        # list of (eos,x,y) makes stroke, list of strokes make sentence,
+        # list of sentence make total textlines in the file
+        # i.e; strokes_wrt_texlines.
+
+        assert len(strokes_wrt_textlines) == len(textslines),\
+            f"Strokes Segregation doesn't match with textlines "\
+            f"{len(strokes_wrt_textlines)} != {len(textslines)}"
+
+        # We want list of (eos, x, y) wrt textlines,
+        # hence unravel list of strokes
+        textlines_seq = []
+        for line in strokes_wrt_textlines:
+            indiv_line_seq = []
+            for strokes in line:
+                indiv_line_seq += strokes
+            indiv_line_seq = np.asarray(indiv_line_seq, dtype=np.float32)
+            indiv_line_seq = change_coord_to_offsets(indiv_line_seq)
+            textlines_seq.append(indiv_line_seq)
+
+        strokes_all.extend(textlines_seq)
+        sentences_all.extend(textslines)
+        print(f'[{file_no:4d}] File: ./{file} -- '
+              f'TextLines: {len(textlines_seq)}')
+
+    assert len(sentences_all) == len(strokes_all)
+
+    return strokes_all, sentences_all
+
+
+if __name__ == "__main__":
+    strokes, sents = extract_data()
