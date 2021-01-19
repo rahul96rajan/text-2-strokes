3c696ae512d6d8e8c54c1a5a06a65e1f11af9006
lowkey96 <lokeshbisht.096@gmail.com>
2021-01-19 20:59:53 +0530

    Removed redundant files

--- GUI/preload.js
+++ GUI/preload.js
@@ -1,24 +1,3 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 const { remote } = require("electron");
-// const {
-//   getCurrentWindow,
-//   openMenu,
-//   minimizeWindow,
-//   maximizeWindow,
-//   unmaximizeWindow,
-//   maxUnmaxWindow,
-//   isWindowMaximized,
-//   closeWindow
-// } = require("./menu-functions");
-
-// window.addEventListener("DOMContentLoaded", () => {
-//   window.getCurrentWindow = getCurrentWindow;
-//   window.openMenu = openMenu;
-//   window.minimizeWindow = minimizeWindow;
-//   window.maximizeWindow = maximizeWindow;
-//   window.unmaximizeWindow = unmaximizeWindow;
-//   window.maxUnmaxWindow = maxUnmaxWindow;
-//   window.isWindowMaximized = isWindowMaximized;
-//   window.closeWindow = closeWindow;
-// });
